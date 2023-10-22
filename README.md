629efbd0e2c76e77c4ad89ec1bf566373d0ddc63
Gilda Perretta <perretta.gilda@gmail.com>
2023-10-22 11:40:50 +0200

    Small code fix + update readme

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 <h1>Arcade - HTML/CSS/Javascript mini-projects</h1> 
 
-<img src="https://github.com/gperretta/arcade-mini-projects/assets/113616815/2e46c2d2-6d40-4183-9f01-cee9fb769f29"/>
+<h3>Running Duck</h3>
+
+<p>
+    First arcade game: <i>infinite runner</i>.<br>
+    What happens when you're a duck trying to avoid a buoy that's floating right in your direction? You <b>jump</b>!<br>
+</p>
+<p>
+    The game area is structured as a grid: each grid cell is a div.<br>
+    While the duck will stay floating in the same point, the buoy will move one cell at a time.<br>
+    You can avoid crashing into the obstacle by tapping the space bar and making the duck jump - thanks to the CSS transform property.<br>
+    The progress will be easily tracked with:
+    <ul>
+        <li>the score: increased by ten per each obstacled that's been avoided</li> 
+        <li>the level and the speed of the buoy: both increased by one each time the score will reach a value that's a multiple of 50</li>
+     </ul><br>
+</p>
+
+<img src="common/screenshots/running-duck-screen.png"/>
 
 
  
--- running-duck/scripts/script.js
+++ running-duck/scripts/script.js
@@ -1,32 +1,31 @@
-const gridCell = document.querySelectorAll('.grid > div');
-const scoreText = document.querySelector('#score');
+const gridCell = document.querySelectorAll('.grid > div');  // select by class
+const scoreText = document.querySelector('#score');         // select by id
+const levelText = document.querySelector('#level');
 
 const duckIndex = 1;
 const duck = gridCell[duckIndex];
 duck.classList.add('duck');
 
 let speed = 3;
-let score = -1;
+let score = 0;
+let level = 1;
 
 function addObstacle() {
     let currObstacleIndex = gridCell.length - 1;
     gridCell[currObstacleIndex].classList.add('buoy');
 
-    const obstacleIndexVal = setInterval(function () {
-        if (score % 50 === 0) {
-            speed = speed++;
-        }
+    scoreText.innerText = score;
+    levelText.innerText = level;
 
+    const obstacleIndexVal = setInterval(function () {
         gridCell[currObstacleIndex].classList.remove('buoy');
         currObstacleIndex--;
-
         // handle exception for negative index values
         if (currObstacleIndex < 0) {
             clearInterval(obstacleIndexVal);
             addObstacle();
             return;
         }
-
         // handle overlapping div (duck & obstacle)
         if (currObstacleIndex === duckIndex &&
             !gridCell[currObstacleIndex].classList.contains('jumping-duck')) {
@@ -36,11 +35,14 @@ function addObstacle() {
             alert("IT'S A CRASH: GAME OVER!");
             return;
         }
-
         gridCell[currObstacleIndex].classList.add('buoy');
     }, 1000 / speed);
-    score++;
-    scoreText.innerText = score;
+    score += 10;
+    // if the score value is a multiple of 50
+    if (score % 50 === 0) {
+        speed += 1;
+        level += 1;
+    }
 }
 addObstacle();
 
